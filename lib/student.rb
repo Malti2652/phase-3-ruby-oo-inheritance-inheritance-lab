3073e05aa31bf584de102cc1fb3e8d0f824c0ed1
Eddington Rowellis <eddingtonibi@gmail.com>
2023-04-02 10:57:42 +0300

    commit tree b5be13a6a8eadcaec29c13154021d3c4bfd10e80

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,8 @@
-class Student 
-
+class Student < User
+  def initialize(first_name, last_name)
+    super(first_name, last_name)
+    @knowledge = []
+  end
+  
+  # rest of the code...
 end
--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,12 @@
-class Teacher
+class Teacher < User
+  KNOWLEDGE = [
+    "The sky is blue",
+    "Two plus two equals four",
+    "Grass is green",
+    "Water is composed of two hydrogen atoms and one oxygen atom"
+  ]
 
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-
-  
+  def teach
+    KNOWLEDGE.sample
+  end
 end
